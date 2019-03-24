ee5d2ebe692f1da26e9d3f39c5fd84af975d4fa1
gadkiyabram <alex-abram@bk.ru>
2019-03-24 15:51:01 +0500

    function nextState moved at the bottom

--- segment-tree.js
+++ segment-tree.js
@@ -1,61 +1,72 @@
-var t = new Array(10);
-
-function build(v, vleft, vright, array) {
-    if (vleft == vright){
-        t[v] = array[vleft];
-        return;
-    }
-
-    let vmiddle = vleft + (vright - vleft) / 2;
-    build(2 * v + 1, vleft, vmiddle, array);
-    build(2 * v + 2, vmiddle + 1, vright, array);
-    t[v] = t[2 * v + 1] + t[2 * v + 2];
-}
-
-function query(v, vleft, vright, from, to, fn, N) {
-    if (to < vleft || vright < from){ return 0; }
-    if (vleft >= from && vright <= to) { return fn(N, t[vleft]); }
-    let vmiddle = vleft + (vright - vleft) / 2;
-    let qleft = query(2 * v + 1, vleft, vmiddle, from, to, fn, N);
-    let qright = query(2 * v + 2, vmiddle + 1, vright, from, to, fn, N);
-    return fn(N, (qleft + qright));
-}
-
 function dummySegmentTree(array, fn, N) {
-
     return function (from, to) {
         let result = N;
-
         for (let i = from; i < to; i++) {
             result = fn(result, array[i]);
         }
-
         return result;
     }
 }
+// function recursiveSegmentTree(array, fn, N) {
+//
+//     return function neutralTree(from, to) {
+//
+//         if (from < 0 || array.length < to){
+//             throw new Error("Out of range");
+//         }
+//         if (to < from) {
+//             throw new Error("From is greater");
+//         }
+//         if (from == to) { return N; }
+//
+//         let result = N;
+//
+//         array.slice(from, to).forEach((arr) => {
+//             if (!Array.isArray(arr)) {
+//                 result += arr;
+//             }
+//             else {
+//                 result += fn(result, arr);
+//             }
+//         });
+//
+//         // console.log(result);
+//
+//         return result;
+//     };
+// };
 
 function segmentTree(array, fn, N) {
+    var t = new Array();
+    function build(v, vleft, vright, array) {
+        if (vleft == vright){
+            t[v] = array[vleft];
+            return;
+        }
 
-    // return function neutralTree(from, to){
-    //
-    //     if (from < 0 || array.length < to){
-    //         throw new Error("Out of range");
-    //     }
-    //     if (to < from) {
-    //         throw new Error("From is greater");
-    //     }
-    //     if (from == to) { return N; }
-    //     let result = N;
-    //
-    //     for (let i = from; i < to; i++) {
-    //         result = fn(result, array[i]);
-    //     }
-    //
-    //     return result;
-    // };
+        let vmiddle = Math.floor(vleft + (vright - vleft) / 2);
+        build(2 * v, vleft, vmiddle, array);
+        build(2 * v + 1, vmiddle + 1, vright, array);
+        t[v] = fn(t[2 * v], t[2 * v + 1]);
+        return t;
+    }
+    
+    function query(v, vleft, vright, from, to, N) {
+        if (to < vleft || vright < from){
+            return N;
+        }
+        if (vleft >= from && vright <= to){
+            return t[v];
+        }
+        let vmiddle = Math.floor(vleft + (vright - vleft) / 2);
+        let ql = query(2 * v, vleft, vmiddle, from, to, N);
+        let qr = query(2 * v + 1, vmiddle + 1, vright, from, to, N);
+        let res = fn(ql, qr);
+        return fn(N, res);
+    }
     return function (from, to) {
-
         if (from < 0 || array.length < to){
+
             throw new Error("Out of range");
         }
         if (to < from) {
@@ -63,16 +74,13 @@ function segmentTree(array, fn, N) {
         }
         if (from == to) { return N; }
 
+        t = build(1, 0, array.length - 1, array);
+
         let result = N;
 
-        return query(0, 0, array.length - 1, from, to, fn, N);
-        // return result;
+        result = query(1, 0, array.length - 1, from, to - 1, N);
+        return result;
     };
-
-};
-
-function nextState(state, assignment, elves, gems) {
-    return state;
 };
 
 function recursiveSegmentTree(array, fn, N) {
@@ -98,8 +106,6 @@ function recursiveSegmentTree(array, fn, N) {
             }
         });
 
-        // console.log(result);
-
         return result;
     };
 };
@@ -118,4 +124,8 @@ function assignAtLeastOne(tree, wishes, stash, elves, gems, week) {
 
 function assignPreferredGems(tree, wishes, stash, elves, gems) {
     return {};
+};
+
+function nextState(state, assignment, elves, gems) {
+    return state;
 };
